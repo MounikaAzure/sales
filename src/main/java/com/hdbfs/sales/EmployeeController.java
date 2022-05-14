9b72c03d035533b475a1ebffe5c9bbba3303dd9c
MounikaAzure <mounika.azure2022@gmail.com>
2022-05-14 15:46:12 +0530

    updateEmployee endpoint impl

--- src/main/java/com/hdbfs/sales/EmployeeController.java
+++ src/main/java/com/hdbfs/sales/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.hdbfs.sales;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,17 @@ public class EmployeeController {
         database.put(emp.getEmpNo(),emp);
         return emp;
     }
+
+    @PostMapping("/updateEmployee")
+    public ResponseEntity<Emp> update(@RequestBody Emp emp){
+        if(emp.getEmpNo() == 0){
+            throw new RuntimeException("No employee found");
+        }
+        int empNo = emp.getEmpNo();
+        database.put(empNo,emp);
+        return ResponseEntity.ok(emp);
+    }
+
 
     @GetMapping("/get-all-employees")
     public Collection<Emp> getAllEmployees(){
